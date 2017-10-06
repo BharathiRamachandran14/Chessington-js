bab6a1e133c8c391823c02652406401c23eb8ba8
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:33:06 +0100

    Pawn passed tests

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,6 +1,7 @@
 import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
+import Board from '../board';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -10,18 +11,38 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         let moves = [];
-        if (this.player === Player.WHITE) {
-            if (location.row === 1) {
-                moves.push(Square.at(location.row + 2, location.col))
-            } 
-             moves.push(Square.at(location.row + 1, location.col))
-            
-        } else {
-            if (location.row === 6){
-                moves.push(Square.at(location.row - 2, location.col))
+
+        // Depending on the pawns current  location
+        // get the 2 squares ahead first and then see if a piece is on either square
+        // if the next 1 is not occupied  moves = [null]
+        // if the 2nd next is occupied return moves = [one move]
+
+
+
+
+        if (this.player === Player.WHITE) { //todo could possibly use a switch here if no need for player.
+
+            if (!board.getPiece(Square.at(location.row + 1, location.col))) {
+
+                if (location.row === 1) {
+                    if (!board.getPiece(Square.at(location.row + 2, location.col))) {
+                        moves.push(Square.at(location.row + 2, location.col));
+                    }
+                }
+                moves.push(Square.at(location.row + 1, location.col));
             }
-            moves.push(Square.at(location.row - 1, location.col))
-        } 
+        } else {
+
+            if (!board.getPiece(Square.at(location.row - 1, location.col))) {
+                if (location.row === 6) {
+                    if (!board.getPiece(Square.at(location.row - 2, location.col))) {
+                        moves.push(Square.at(location.row - 2, location.col));
+                    }
+                }
+                moves.push(Square.at(location.row - 1, location.col));
+            }
+        }
+
         return moves;
     }
 }
