429bf43fe57087498d5f56396bd9283f6daa39fd
Konstantinos <kon.xen@gmail.com>
2022-08-17 16:16:44 +0100

    something wrong with condition

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,29 +1,63 @@
 import Piece from './piece';
 import Player from "../player";
 import Square from "../square";
+import Board from "../board";
 
 export default class Rook extends Piece {
     constructor(player) {
         super(player);
     }
 
+    checkIfFriendlies(board, square) {
+        if (board.getPiece(square) === this.player.symbol) {
+            return true;
+        }
+    }
+
     getAvailableMoves(board) {
 
         let location = board.findPiece(this);
         let moves = [];
+
         for (let i = 1; i <= (7 - location.row); i++) {
-            moves.push(Square.at(location.row + i, location.col)); // moves up
+            let square = Square.at(location.row + i, location.col);
+            if (this.checkIfFriendlies(board, square)) {
+                break;
+            } else {
+                moves.push(square); // moves up}
+            }
         }
         for (let i = 1; i <= (location.row); i++) {
-            moves.push(Square.at(location.row - i, location.col)); // moves down
+            let square = Square.at(location.row - i, location.col);
+            if (this.checkIfFriendlies(board, square)) {
+                break;
+            } else {
+                moves.push(square); // moves down}
+            }
         }
         for (let i = 1; i <= (7 - location.col); i++) {
-            moves.push(Square.at(location.row, location.col + i)); // moves right
+            let square = Square.at(location.row, location.col + i);
+
+            if (this.checkIfFriendlies(board, square)) {
+                break;
+            } else {
+                moves.push(square);
+            } // moves right;}
         }
+
         for (let i = 1; i <= (location.col); i++) {
-            moves.push(Square.at(location.row, location.col - i)); // moves left
+
+            let square = Square.at(location.row, location.col - i);
+
+            if (this.checkIfFriendlies(board, square)) {
+                break;
+            } else {
+                moves.push(square); // moves left;}
+            }
+
         }
         return moves;
     }
 }
+
 
