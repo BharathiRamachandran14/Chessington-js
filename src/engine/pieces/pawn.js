1bce3c6846aea4a5fcc80b7884d286986b4bb5a3
Konstantinos <kon.xen@gmail.com>
2022-08-17 12:11:17 +0100

    Pawn passed tests tidy

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -12,14 +12,6 @@ export default class Pawn extends Piece {
         let location = board.findPiece(this)
         let moves = [];
 
-        // Depending on the pawns current  location
-        // get the 2 squares ahead first and then see if a piece is on either square
-        // if the next 1 is not occupied  moves = [null]
-        // if the 2nd next is occupied return moves = [one move]
-
-
-
-
         if (this.player === Player.WHITE) { //todo could possibly use a switch here if no need for player.
 
             if (!board.getPiece(Square.at(location.row + 1, location.col))) {
