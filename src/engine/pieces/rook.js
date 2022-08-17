2d2dfb88c8f04810fc3c96fa52a215732a3f51e5
Konstantinos <kon.xen@gmail.com>
2022-08-17 10:42:38 +0100

    Rooks passed tests

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -8,57 +8,22 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        //6 rows and 6 columns
-        // if rook modle of the board +nO of rows  /
-        //16 squares from 0
+
         let location = board.findPiece(this);
         let moves = [];
-        // if (this.player === Player.WHITE) {
-            for (let i = 1; i <= (7 - location.row); i++) {
-                moves.push(Square.at(location.row + i, location.col)); // moves up
-            }
-            for (let i = 1; i <= (location.row); i++) {
-                moves.push(Square.at(location.row - i, location.col)); // moves down
-            }
-            for (let i = 1; i <= (7 - location.column); i++) {
-                moves.push(Square.at(location.row, location.col+i)); // moves right
-            }
-            for (let i = 1; i <= (location.column); i++) {
-                moves.push(Square.at(location.row, location.col-i)); // moves left
-            }
-        // }
-        // else {
-        //     for (let i = 1; i <= (7 - location.row); i++) {
-        //         moves.push(Square.at(location.row + i), location.col);
-        //     }
-        //     for (let i = 1; i <= (location.row); i++) {
-        //         moves.push(Square.at(location.row - i), location.col);
-        //     }
-        //     for (let i = 1; i <= (7 - location.column); i++) {
-        //         moves.push(Square.at(location.row), (location.col+i));
-        //     }
-        //     for (let i = 1; i <= (location.column); i++) {
-        //         moves.push(Square.at(location.row), (location.col-i));
-        //     }
-        // }
-
-
-            // 
-            //     if (location.row === 0) {
-            //         moves.push(Square.at(location.row + 7, location.col));
-            //     }
-            //     //  location - ( squares - location)  or location + 3
-            //     moves.push(Square.at(location.row + 1, location.col));
-
-            // } else {
-            //     if (location.row === 6){
-            //         moves.push(Square.at(location.row - 2, location.col));
-            //     }
-            //     moves.push(Square.at(location.row - 1, location.col));
-            // }
-            return moves;
-
-            //return new Array(0);
+        for (let i = 1; i <= (7 - location.row); i++) {
+            moves.push(Square.at(location.row + i, location.col)); // moves up
         }
+        for (let i = 1; i <= (location.row); i++) {
+            moves.push(Square.at(location.row - i, location.col)); // moves down
+        }
+        for (let i = 1; i <= (7 - location.col); i++) {
+            moves.push(Square.at(location.row, location.col + i)); // moves right
+        }
+        for (let i = 1; i <= (location.col); i++) {
+            moves.push(Square.at(location.row, location.col - i)); // moves left
+        }
+        return moves;
     }
+}
 
