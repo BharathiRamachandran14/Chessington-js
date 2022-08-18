7e529a0ce3df4624c15ba913b5de750f03c909e0
Bharathi Ramachandran <Bharathi.Ramachandran@softwire.com>
2022-08-18 08:54:48 +0100

    bishop made to move

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,21 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let moves = [];
+        for (let i = 1; i < (7 - location.row); i++) {
+            moves.push(Square.at(location.row + i, location.col + i));
+        }
+        for (let i = 1; i <= (location.row); i++) {
+            moves.push(Square.at(location.row - i, location.col - i));
+        }
+        for (let i = 1; i < (7 - location.col); i++) {
+            moves.push(Square.at(location.row + i, location.col - i));
+        }
+        for (let i = 1; i < (location.col); i++) {
+            moves.push(Square.at(location.row - i, location.col + i));
+        }
+
+        return moves;
     }
 }
